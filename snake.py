a9a6ee1ea5684358f5123019d6778dbb57939387
yaara21-meet <yaara21@meet.mit.edu>
2019-07-29 10:07:06 -0400

    Stopping here

--- snake.py
+++ snake.py
@@ -25,24 +25,6 @@ snake.shape("square")
 turtle.hideturtle()
 
 
-#Draw a snake at the start of the game with a for loop
-#for loop should use range() and count up to the number of pieces
-#in the snake (i.e. START_LENGTH)
-for size_turtle  in range(START_LENGTH) :
-    x_pos=snake._____________ #Get x-position with snake.pos()[0]
-    y_pos=snake._____________ 
-
-    #Add SQUARE_SIZE to x_pos. Where does x_pos point to now?    
-    # You're RIGHT!
-    x_pos+=__________ 
-
-    snake.goto(____________,____________) #Move snake to new (x,y)
-   
-    #Now draw the new snake part on the screen (hint, you have a 
-    #function to do this
-    _____________()
-
-
 
 #Function to draw a part of the snake on the screen
 def new_stamp():
@@ -50,5 +32,27 @@ def new_stamp():
     pos_lost.append(snake_pos)        
     snake_stanp = snake.stamp()
     stamp_list.append(snake_pos)
+
+
+#Draw a snake at the start of the game with a for loop
+#for loop should use range() and count up to the number of pieces
+#in the snake (i.e. START_LENGTH)
+for size_turtle  in range(START_LENGTH) :
+    x_pos = snake.pos() #Get x-position with snake.pos()[0]
+    y_pos = snake.pos() 
+
+    #Add SQUARE_SIZE to x_pos. Where does x_pos point to now?    
+    # You're RIGHT!
+    x_pos += SQUARE_SIZE 
+
+    snake.goto(x_pos,y_pos) #Move snake to new (x,y)
+   
+    #Now draw the new snake part on the screen (hint, you have a 
+    #function to do this
+    new stamp()
+
+
+
+
 
 
